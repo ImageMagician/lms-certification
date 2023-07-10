ff8889c7b2d75d3cd9e50b21361d0e3fc76df1d0
Mark Larson (Lion Energy) <mark.larson@lionenergy.com>
2023-07-10 16:21:05 -0600

    app.blade.php vite change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'node_modules/bootstrap/js/src/tab.js','public/js/quizzes.js?r=1'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'public/js/quizzes.js?r=1' /*, 'node_modules/bootstrap/js/src/tab.js' */])
 </head>
 <body>
     <div id="app">
