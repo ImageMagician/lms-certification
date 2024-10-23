f416ec982238230f704cf9723fb4f8bf67a20136
ImageMagician <snippets@freeform-studios.com>
2024-10-23 14:57:59 -0600

    create_users_table migration: add address fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
             $table->text('companies');
             $table->string('states');
             $table->string('password');
